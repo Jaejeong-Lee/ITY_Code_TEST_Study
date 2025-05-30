e67a64f3902d66730b913cbbf2151deb86783e2d
Jaejeong Lee <leejae938@gmail.com>
2025-05-30 11:40:37 +0900

    [level 3] Title: 단어 변환, Time: 23.58 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,78 +1,69 @@
 import java.util.*;
-
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        // 애초에 target이 words에 없으면 return 0
+        
         Set<String> set = new HashSet<>(Arrays.asList(words));
         if (!set.contains(target)) return 0;
-
-        // 인접 리스트 생성
+        
         Map<String, List<String>> map = new HashMap<>();
-        // begin은 words에 없으므로 인접리스트 생성시 추가해줌
-        for (String w : words) {
+        for (String w : words){
             map.put(w,new ArrayList<>());
         }
-        map.put(begin, new ArrayList<>());
-
-        //begin의 인접 리스트 생성
-        for (String word : words) {
-            if (isOneDiff(word,begin)) {
+        map.put(begin,new ArrayList<>());
+        
+        for (String word : words){
+            if (isOneDiff(begin, word)){
                 map.get(begin).add(word);
                 map.get(word).add(begin);
             }
         }
-
-        for (String word : words) {
-            for (String w : words) {
-                // 인접 리스트 생성
-                // 한 글자만 다르면 인접 리스트에 추가
-                // addList() : 1글자만 다를 때 true 반환, 나머지 경우 false
-                if (isOneDiff(word,w)) {
-                    map.get(word).add(w);
-                    map.get(w).add(word);
+        
+        for (String w1 : words){
+            for (String w2 : words){
+                if (isOneDiff(w1,w2)){
+                    map.get(w1).add(w2);
+                    map.get(w2).add(w1);
                 }
             }
         }
-        // BFS
-        // 초깃값 세팅
-        Queue<Map.Entry<String,Integer>> queue  = new ArrayDeque<>();
+        
+        Queue<Map.Entry<String,Integer>> queue = new ArrayDeque<>();
         Set<String> visited = new HashSet<>();
         visited.add(begin);
-        queue.offer(new AbstractMap.SimpleEntry<>(begin,0));
-
-        while (!queue.isEmpty()) {
-            //현재 노드 방문 : 값 꺼내기
+        queue.offer(new AbstractMap.SimpleEntry(begin,0));
+        
+        while (!queue.isEmpty()){
             Map.Entry<String,Integer> temp = queue.poll();
             Integer time = temp.getValue();
             String curr = temp.getKey();
-
+            
             if (curr.equals(target)) return time;
-
-            for (String next : map.get(curr)) {
+            
+            for (String next : map.get(curr)){
                 if (!visited.contains(next)){
-                    // 다음 노드 방문
                     visited.add(next);
-                    queue.offer(new AbstractMap.SimpleEntry<>(next,time+1));
+                    queue.offer(new AbstractMap.SimpleEntry(next,time+1));
                 }
             }
         }
+        
+        
         return 0;
     }
-    public static boolean isOneDiff(String word, String w) {
-        String[] temp = word.split("");
-        String[] tempW = w.split("");
-        // 같은 글자면 체크 안 하고 false
-        if (word.equals(w)) return false;
-
+    
+    public static boolean isOneDiff(String w1, String w2){
+        String[] temp1 = w1.split("");
+        String[] temp2 = w2.split("");
+        
+        if (w1.equals(w2)) return false;
+        
         int count = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (temp[i].equals(tempW[i])) count++;
+        for (int i = 0 ; i < temp1.length ; i++){
+            if (temp1[i].equals(temp2[i])) count++;
         }
-        // 인접 리스트 생성
-        // 한 글자만 다르면 인접 리스트에 추가
-        // true 반환
-        if (count == word.length() - 1) return true;
-
+        if (count == w1.length() - 1) return true;
+        
         return false;
     }
+    
 }
